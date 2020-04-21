3696656b60a59232e2649c95754a14f445df058f
sethl <lewandowski.seth@gmail.com>
2020-04-21 15:46:54 -0700

    fixed for mobile

--- src/App.js
+++ src/App.js
@@ -55,19 +55,19 @@ class App extends React.Component {
     const markdown = marked(text, {sanitize: true});
   
     return (
-      <div className="App-header column">
-        <div id="container" className="">
+      <div className="App-header">
+        <div id="title" className="col-12">
           <h1>Convert Markdown</h1>
-          <div className="row">
-            <div className="col-sm-12 col-md-6">
-              <h3>Enter Markdown</h3>
-              <textarea className="form-control" id="editor" value={text} onChange={this.handleChange}/>
-            </div>
-            <div className="col-sm-12 col-md-6" id="">
-              <h3>See Result</h3>
-              <div className="rounded preview">
-                <div id="preview" dangerouslySetInnerHTML={this.getMarkdownText()}></div>
-              </div>
+        </div>
+        <div className="row">
+          <div id="markdown" className="col-sm-12 col-md-6">
+            <h3>Enter Markdown</h3>
+            <textarea className="form-control" id="editor" value={text} onChange={this.handleChange}/>
+          </div>
+          <div id="result" className="col-sm-12 col-md-6">
+            <h3>See Result</h3>
+            <div className="rounded preview">
+              <div id="preview" dangerouslySetInnerHTML={this.getMarkdownText()}></div>
             </div>
           </div>
         </div>
